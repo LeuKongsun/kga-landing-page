492a248308a65204a97df160164c388069c32e2d
Kongsun Leu <kongsun.l@pipay.com>
2024-08-12 10:46:30 +0700

    fix(default-theme): updated to default theme light

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -15,7 +15,7 @@ const Layout = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel='icon' href='/favicon.ico' /> */}
       </Head>
-      <ThemeProvider attribute="class">
+      <ThemeProvider attribute="class" defaultTheme="light">
         <div className="dark:bg-gradient-dark">
           <Navbar />
           <main>{children}</main>
