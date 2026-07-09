04e650d233e5d5d4b6cdd66fb74f396a0308ab35
sophon-pro <sophonthoy1998@gmail.com>
2026-07-09 14:47:25 +0700

    update drop what we teach strip

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import dynamic from "next/dynamic";
 import Hero from "./components/ui/Hero";
 import GradientWrapper from "./components/GradientWrapper";
 
-const LogoGrid = dynamic(() => import("./components/ui/LogoGrid"), { ssr: true });
 const Courses = dynamic(() => import("./components/ui/Courses"), { ssr: true });
 const CTA = dynamic(() => import("./components/ui/CTA"), { ssr: true });
 const ToolKit = dynamic(() => import("./components/ui/ToolKit"), { ssr: true });
@@ -16,7 +15,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <LogoGrid />
       <GradientWrapper>
         <Courses />
         <CTA />
